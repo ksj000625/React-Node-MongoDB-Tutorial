8df87fc1856879602b31fdff4c3377c4a9a6a91b
Kim Seongjin <darkmoon625@naver.com>
2023-12-25 00:54:51 +0900

    Create SideVideo Tab

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 
+import SideVideo from "./Sections/SideVideo";
+
 function VideoDetailPage(props) {
 	const videoId = props.match.params.videoId;
 	const variable = {
@@ -42,7 +44,7 @@ function VideoDetailPage(props) {
 					</div>
 				</Col>
 				<Col lg={6} xs={24}>
-					Side Videos
+					<SideVideo />
 				</Col>
 			</Row>
 		);
