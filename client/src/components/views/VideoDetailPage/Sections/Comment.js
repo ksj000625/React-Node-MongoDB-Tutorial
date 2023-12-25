f6f2afbfd387cb1f21887491387bc6210c9eabec
Kim Seongjin <darkmoon625@naver.com>
2023-12-25 13:27:16 +0900

    Create comment model & section

---
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comment() {
+	return (
+		<div>
+			<div>Comment</div>
+		</div>
+	);
+}
+
+export default Comment;
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,6 +4,7 @@ import Axios from "axios";
 
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
+import Comment from "./Sections/Comment";
 
 function VideoDetailPage(props) {
 	const videoId = props.match.params.videoId;
@@ -50,6 +51,7 @@ function VideoDetailPage(props) {
 							/>
 						</List.Item>
 						{/** Comments */}
+						<Comment />
 					</div>
 				</Col>
 				<Col lg={6} xs={24}>
