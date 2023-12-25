fcc531d425e5986d0b870262258f07bf1ae416cd
Kim Seongjin <darkmoon625@naver.com>
2023-12-25 13:13:43 +0900

    Correct Subscription
    Change logic that can subscribe when userTo !== userFrom

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -24,6 +24,14 @@ function VideoDetailPage(props) {
 	}, []);
 
 	if (VideoDetail.writer) {
+		const subscribeButton = VideoDetail.writer._id !==
+			localStorage.getItem("userId") && (
+			<Subscribe
+				userTo={VideoDetail.writer._id}
+				userFrom={localStorage.getItem("userId")}
+			/>
+		);
+
 		return (
 			<Row gutter={[16, 16]}>
 				<Col lg={18} xs={24}>
@@ -34,14 +42,7 @@ function VideoDetailPage(props) {
 							controls
 						/>
 
-						<List.Item
-							actions={[
-								<Subscribe
-									userTo={VideoDetail.writer._id}
-									userFrom={localStorage.getItem("userId")}
-								/>,
-							]}
-						>
+						<List.Item actions={[subscribeButton]}>
 							<List.Item.Meta
 								avatar={<Avatar src={VideoDetail.writer.image} />}
 								title={VideoDetail.writer.name}
