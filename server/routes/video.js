f499974a4a5b78799c7bc1f8ebf522789eca6626
Kim Seongjin <darkmoon625@naver.com>
2023-12-25 13:02:57 +0900

    Create subscriptionPage
    Subscriptions Page shows videos whose you subscribed

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const { Video } = require("../models/Video");
 
 const multer = require("multer");
 var ffmpeg = require("fluent-ffmpeg");
+const { Subscriber } = require("../models/Subscriber");
 
 // STORAGE MULTER CONFIG
 let storage = multer.diskStorage({
@@ -116,6 +117,30 @@ router.post("/getVideoDetail", (req, res) => {
 			if (err) return res.status(400).send(err);
 			return res.status(200).json({ success: true, videoDetail });
 		});
+});
+
+// SubscriptionPage - 구독한 비디오 랜딩 관련
+router.post("/getSubscriptionVideos", (req, res) => {
+	// 자신의 아이디를 가지고 구독하는 사람들을 찾는다.
+	Subscriber.find({ userFrom: req.body.userFrom }).exec(
+		(err, subscriberInfo) => {
+			if (err) return res.status(400).send(err);
+
+			let subscribedUser = [];
+
+			subscriberInfo.map((subscriber, i) => {
+				subscribedUser.push(subscriber.userTo);
+			});
+
+			// 찾은 사람들의 비디오를 가지고 온다.
+			Video.find({ writer: { $in: subscribedUser } })
+				.populate("writer")
+				.exec((err, videos) => {
+					if (err) return res.status(400).send(err);
+					return res.status(200).json({ success: true, videos });
+				});
+		}
+	);
 });
 
 module.exports = router;
