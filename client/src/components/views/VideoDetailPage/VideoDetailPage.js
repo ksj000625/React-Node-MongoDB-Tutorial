843e0f36ff0552986ddb4050f776b07c1d4ff71b
Kim Seongjin <darkmoon625@naver.com>
2023-12-25 12:13:02 +0900

    UI&Routes setting for Subscribe

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 
 import SideVideo from "./Sections/SideVideo";
+import Subscribe from "./Sections/Subscribe";
 
 function VideoDetailPage(props) {
 	const videoId = props.match.params.videoId;
@@ -33,7 +34,9 @@ function VideoDetailPage(props) {
 							controls
 						/>
 
-						<List.Item actions>
+						<List.Item
+							actions={[<Subscribe userTo={VideoDetail.writer._id} />]}
+						>
 							<List.Item.Meta
 								avatar={<Avatar src={VideoDetail.writer.image} />}
 								title={VideoDetail.writer.name}
